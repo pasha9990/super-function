788bc992f289217e2ecf94bba916ff32892dd95e
pasha <pashap9990@gmail.com>
2020-02-11 18:52:31 +0330

    add substring function

--- processBar.js
+++ processBar.js
@@ -7,39 +7,46 @@ const unqiueValues = function unqiueValues(arr) {
 const superFunctionalStrings = function superFunctionalStrings(str) {
     let p = str.length;
     let d = (unqiueValues(str.split(''))).length;
-    return Math.pow(p, d) % ((Math.pow(10, 0)) + 7);
+    
+    return Math.pow(p, d) % (Math.pow(10,9)+ 7);
 }
 
-var tree = function tree(leafs) {
-    var branches = [];
-    if (leafs.length == 1) return leafs;
-    for (var k in leafs) {
-        var leaf = leafs[k];
-        tree(leafs.join('').replace(leaf, '').split('')).concat("").map(function (subtree) {
-            branches.push([leaf].concat(subtree));
-        });
-    }
-    return branches;
-};
+const subString = function subString(str, n)  
+{ 
 
-let str = 'aba';
+    let result = [];
+    // Pick starting point 
+    for (len = 1; len <= n; len++)  
+    {  
+          
+        // Pick ending point 
+        for (let i = 0; i <= n - len; i++)  
+        { 
+              
+            // Print characters from current 
+            // starting point to current ending 
+            // point.  
+            let j = i + len - 1;          
+            res = '';
+            for (let k = i; k <= j; k++)  
+                res +=(str[k]); 
+            result.push(res);
+        } 
+    } 
+    return result;
+} 
 
-let result = tree(str.split('')).map(function (str) {
-    return str.join('');
-});
+let str = 'abc';
+let result = unqiueValues((subString(str,str.length)));
 
 
-result = unqiueValues(result);
 console.log(result);
 let sum = 0;
+
 for (let item of result) {
     sum +=superFunctionalStrings(item);
 
 }
 
+
 console.log(sum);
-// let result = [];
-// for(let i = str.length ; i > 0 ; i--){ 
-    // result.push(_generateString(chars,'',2));
-// }
-// console.log(result);
